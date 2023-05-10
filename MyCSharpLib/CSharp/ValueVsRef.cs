8a482112e5c48016bc881a2fa4ed0736cdc83569
Chintan Joshi <chintan.joshi@hubparking.com>
2023-05-10 10:31:48 -0400

    Value vs Ref

---
+++ MyCSharpLib/CSharp/ValueVsRef.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpLib.CSharp;
+
+public class ValueVsRef
+{
+    public ValueVsRef()
+    {
+        
+    }
+
+    public ValueVsRef(int value)
+    {
+        Value = value;
+    }
+
+    public int Value { get; set; }
+}
+
+public class ModifyObj
+{
+    public void ModifyValue(ValueVsRef obj)
+    {
+        obj.Value = 20;
+    }
+
+    public void ModifyValue(ref ValueVsRef obj)
+    {
+        obj.Value = 20;
+    }
+
+    public void MakeCopyAndModifyValue(ValueVsRef obj)
+    {
+        obj = new ValueVsRef
+        {
+            Value = 20
+        };
+    }
+
+    public void MakeCopyAndModifyValue(ref ValueVsRef obj)
+    {
+        obj = new ValueVsRef
+        {
+            Value = 20
+        };
+    }
+}
---
+++ MyCSharpLibTests/CSharpTests/ValueVsRefTests.cs
@@ -0,0 +1,47 @@
+﻿using MyCSharpLib.CSharp;
+
+namespace MyCSharpLibTests.CSharpTests;
+
+[TestClass]
+public class ValueVsRefTests
+{
+    private ValueVsRef obj;
+    private ModifyObj mod;
+
+    [TestInitialize]
+    public void Init()
+    {
+        obj = new ValueVsRef(10);
+        mod = new ModifyObj();
+        
+    }
+
+    [TestMethod]
+    public void TestModify1()
+    {
+        mod.ModifyValue(obj);
+        Assert.AreEqual(20, obj.Value);
+    }
+
+    [TestMethod]
+    public void TestModify2()
+    {
+        mod.ModifyValue(ref obj);
+        Assert.AreEqual(20, obj.Value);
+    }
+
+
+    [TestMethod]
+    public void TestMakeCopyAndModify1()
+    {
+        mod.MakeCopyAndModifyValue(obj);
+        Assert.AreEqual(10, obj.Value);
+    }
+
+    [TestMethod]
+    public void TestMakeCopyAndModify2()
+    {
+        mod.MakeCopyAndModifyValue(ref obj);
+        Assert.AreEqual(20, obj.Value);
+    }
+}
