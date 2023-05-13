ac00100c422d97516c9ca5225d7f5aa40fd42b04
Chintan Joshi <chintan.joshi@hubparking.com>
2023-05-13 00:58:51 -0400

    More leetcode questions

---
+++ MyCSharpLib/LeetCode/KidsWithCandies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace MyCSharpLib.LeetCode
+{
+    public class KidsWithCandies
+    {
+        public IList<bool> KidsWithCandiesSol(int[] candies, int extraCandies)
+        {
+            var n = candies.Length;
+            IList<bool> result = new List<bool>(new bool[n]);
+            var max = candies.Max();
+
+            for (int i = 0; i < n; i++)
+            {
+                if (candies[i] + extraCandies > max)
+                {
+                    result[i] = true;
+                }
+            }
+
+
+            candies.Select(x => x + extraCandies > candies.Max()).ToArray();
+
+            return result;
+        }
+    }
+}
---
+++ MyCSharpLibTests/LeetCodeTests/KidsWithCandiesTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCSharpLib.LeetCode;
+
+namespace MyCSharpLibTests.LeetCodeTests
+{
+    [TestClass]
+    public class KidsWithCandiesTests
+    {
+
+        [TestMethod]
+        public void test1()
+        {
+            var sol = new KidsWithCandies();
+            int[] candies = { 2, 8, 7 };
+            int extraCandies = 1;
+
+            var result = sol.KidsWithCandiesSol(candies, extraCandies);
+        }
+    }
+}
