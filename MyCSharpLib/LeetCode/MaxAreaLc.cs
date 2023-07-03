3ab05cfbfb54cf64cab422b99a69b0a3dc31b5f0
Chintan Joshi <chintan.joshi@hubparking.com>
2023-07-03 14:22:25 -0400

    Container With Most Water LC

---
+++ MyCSharpLib/LeetCode/MaxAreaLc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharpLib.LeetCode
+{
+    public class MaxAreaLc
+    {
+        public int MaxArea(int[] height)
+        {
+            var left = 0;
+            var right = height.Length - 1;
+            var ans = 0;
+
+            while (right > left)
+            {
+
+                var temp = (right - left) * Math.Min(height[left], height[right]);
+
+                if (height[left] <= height[right])
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+
+                ans = ans > temp ? ans : temp;
+
+            }
+
+            return ans;
+
+        }
+    }
+}
---
+++ MyCSharpLibTests/LeetCodeTests/MaxAreaLcTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyCSharpLib.LeetCode;
+
+namespace MyCSharpLibTests.LeetCodeTests
+{
+    [TestClass]
+    public class MaxAreaLcTests
+    {
+        [TestMethod]
+        public void MaxAreaTest1()
+        {
+            var sol = new MaxAreaLc();
+            var area =  sol.MaxArea(new[] { 2, 3, 4, 5, 18, 17, 6 });
+            Assert.AreEqual(17, area);
+        }
+    }
+}
